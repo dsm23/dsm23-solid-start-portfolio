75f16ff8e121f61ef09e7ebee2f1e06529726ed1
David Murdoch <dsmurdoch357@gmail.com>
2024-04-05 18:27:10 +0100

    chore(clock): add missing clock styles

--- src/routes/clock.tsx
+++ src/routes/clock.tsx
@@ -1,3 +1,4 @@
+import Anchor from "~/components/anchor";
 import ClockFace from "~/components/clock";
 import Main from "~/components/main";
 import StyledGoBack from "~/components/styled-go-back";
@@ -6,8 +7,11 @@ const Clock = () => (
   <Main class="w-full py-8 px-6">
     <StyledGoBack class="mb-4" href="/#projects" />
 
-    <h1>Clock</h1>
-    <p>A SVG clock example cloned from svelte.dev</p>
+    <h1 class="text-4xl uppercase tracking-widest text-sky-600">Clock</h1>
+    <p>
+      A SVG clock example cloned from{" "}
+      <Anchor href="https://svelte.dev/examples/clock">svelte.dev</Anchor>
+    </p>
 
     <div class="container mx-auto">
       <ClockFace />
